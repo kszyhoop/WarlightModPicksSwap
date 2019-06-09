8ecb4e3c1d9fe20b120e96bafb162b101d3728ae
kszyhoop <jamulear@o2.pl>
2019-06-09 23:36:20 +0200

    No popup with mod info for spectators

--- Client_GameRefresh.lua
+++ Client_GameRefresh.lua
@@ -1,7 +1,7 @@
 function Client_GameRefresh(game)
 	if game.Game.TurnNumber > 0 then return end; -- don't pop up in ongoing games, could be removed later
 
-	if not Mod.PlayerGameData.InitialPopupDisplayed then
+	if not Mod.PlayerGameData.InitialPopupDisplayed and game.Us ~= nil then
 		UI.Alert("This game includes Swap Picks Mod - you pick for your opponent. Good luck :)")
 		local payload = {};
 		payload.Message = "InitialPopupDisplayed";
